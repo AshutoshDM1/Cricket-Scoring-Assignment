11d7456f938f31c0ea9dbecacac3cf121d9bc0d6
AshutoshDM1 <downlodemaster1@gmail.com>
2024-12-11 20:08:31 +0530

    Cricket Scoring App Started

---
+++ Frontend/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Cricket Scoring App",
+  description: "Cricket Scoring App Assignment",
+  icons:
+    "https://res.cloudinary.com/dnvl8mqba/image/upload/v1733927766/Cricket%20Scoring%20App/Cricket_Scoring_App_zekqsn.png",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ Frontend/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Cricket Scoring App Assignment</h1>
+    </div>
+  );
+}
